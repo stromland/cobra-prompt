ff724c1b7d8145402b2b3839f062440ccbbdd053
Espen Strømland <espen.stromland@gmail.com>
2017-08-20 21:59:50 +0200

    coprompt wip

---
+++ README.md
@@ -0,0 +1,13 @@
+#CoPrompt
+
+[WIP]
+
+Connect cobra and go-prompt 
+
+https://github.com/c-bata/go-prompt
+
+https://github.com/spf13/cobra
+
+
+Example
+https://github.com/stromland/coprompt-example
---
+++ coprompt.go
@@ -0,0 +1,79 @@
+package coprompt
+
+import (
+	"strings"
+	"os"
+	"github.com/c-bata/go-prompt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/cobra"
+)
+
+//CoPrompt will check if your command includes the annotation "coprompt". If it's included the value will be
+//provided to the HandleDynamicSuggestions function.
+type HandleDynamicSuggestions func(annotation string, document prompt.Document) []prompt.Suggest
+
+var lRootCmd *cobra.Command
+var lHandleDynamicSuggestions HandleDynamicSuggestions
+
+/*
+CoPrompt will automatically generate suggestions for all your cobra commands and flags
+CoPrompt will check if your command includes the annotation "coprompt". If it's included the value will be
+provided to the HandleDynamicSuggestions function.
+ */
+func Run(rootCmd *cobra.Command, fn HandleDynamicSuggestions, options []prompt.Option) {
+	lRootCmd = rootCmd
+	lHandleDynamicSuggestions = fn
+
+	p := prompt.New(
+		executor,
+		completer,
+		options...
+	)
+	p.Run()
+}
+
+// executor executes command and print the output.
+func executor(in string) {
+	promptArgs := strings.Split(in, " ")
+	os.Args = append([]string{os.Args[0]}, promptArgs...)
+	lRootCmd.Execute()
+}
+
+// completer returns the completion items from user input.
+func completer(d prompt.Document) []prompt.Suggest {
+	currentCommand := lRootCmd
+	cText := d.CurrentLine()
+	args := strings.Fields(cText)
+
+	for _, v := range args {
+		if currentCommand.HasAvailableSubCommands() {
+			for _, c := range currentCommand.Commands() {
+				if c.Name() == v {
+					currentCommand = c
+					break
+				}
+			}
+		} else {
+			break
+		}
+	}
+
+	var suggestions []prompt.Suggest
+	if strings.HasPrefix(d.GetWordBeforeCursor(), "-") {
+		flags := []*pflag.FlagSet{lRootCmd.PersistentFlags(), currentCommand.LocalNonPersistentFlags()}
+		for _, fs := range flags {
+			fs.VisitAll(func(flag *pflag.Flag) {
+				suggestions = append(suggestions, prompt.Suggest{Text: "--" + flag.Name, Description: flag.Usage})
+			})
+		}
+	} else if currentCommand.HasAvailableSubCommands() {
+		for _, c := range currentCommand.Commands() {
+			suggestions = append(suggestions, prompt.Suggest{Text: c.Name(), Description: c.Short})
+		}
+	} else if currentCommand.Annotations["coprompt"] != "" {
+		copromptAnnotation := currentCommand.Annotations["coprompt"]
+		suggestions = lHandleDynamicSuggestions(copromptAnnotation, d)
+	}
+
+	return prompt.FilterHasPrefix(suggestions, d.GetWordBeforeCursor(), true)
+}
